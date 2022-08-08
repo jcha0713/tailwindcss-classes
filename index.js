3f54f9049d7e79788ed1ced029dc4f2d408dae3d
jcha0713 <joocha0713@gmail.com>
2022-08-08 00:01:42 -0700

    Add comments

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import titles from './titles.js'
 
 const excludeSet = new Set(['/', 'Start', 'Between', 'End'])
 
+// For each title in the titles list, check if it contains any word that is in the excludeSet.
+// Capitalize the title and replace whitespaces with hypens
 const modifiedTitles = titles.map(title => {
   return title.split(' ').filter(word => !excludeSet.has(word)).map(word => {
     if (word.includes('-')) {
@@ -16,12 +18,23 @@ const modifiedTitles = titles.map(title => {
   }).join('-')
 })
 
+
+/**
+ * It requires the latest node version as it uses the builtin fetch api
+ *
+ * @param {string} url url of the page
+ * @returns {Promise} text component of the response
+ */
 async function fetchData(url) {
   const response = await fetch(url)
   const body = await response.text()
   return body
 }
 
+/**
+ * @param {string} url url of the page
+ * @returns {Promise} promise object that has tailwindcss class name as key and css code as value
+ */
 async function getTableContents(url) {
   const html = await fetchData(url)
   const $ = cheerio.load(html)
@@ -42,6 +55,7 @@ async function getTableContents(url) {
   return resultMap
 }
 
+/* Construct object for each page and write a JSON file */
 async function writeJSONFile() {
   let classMap = {}
   
