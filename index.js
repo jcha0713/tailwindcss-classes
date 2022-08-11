16eb8cdc5924a6df5975cb08afb96d7d3e1b6543
jcha0713 <joocha0713@gmail.com>
2022-08-10 23:51:14 -0700

    Add prettier

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
-import * as cheerio from 'cheerio'
-import fs from 'fs/promises'
-import titles from './titles.js'
+import * as cheerio from "cheerio";
+import fs from "fs/promises";
+import titles from "./titles.js";
 
-const excludeSet = new Set(['/', 'Start', 'Between', 'End'])
+const excludeSet = new Set(["/", "Start", "Between", "End"]);
 
 // For each title in the titles list, check if it contains any word that is in the excludeSet.
 // Capitalize the title and replace whitespaces with hypens
-const modifiedTitles = titles.map(title => {
-  return title.split(' ').filter(word => !excludeSet.has(word)).map(word => {
-    if (word.includes('-')) {
-      const hyphenIdx = word.indexOf('-')
-      if (word.length !== hyphenIdx) {
-        word = word.slice(0, hyphenIdx) + '-' + word.charAt(hyphenIdx + 1).toLowerCase() + word.slice(hyphenIdx + 2)
+const modifiedTitles = titles.map((title) => {
+  return title
+    .split(" ")
+    .filter((word) => !excludeSet.has(word))
+    .map((word) => {
+      if (word.includes("-")) {
+        const hyphenIdx = word.indexOf("-");
+        if (word.length !== hyphenIdx) {
+          word =
+            word.slice(0, hyphenIdx) +
+            "-" +
+            word.charAt(hyphenIdx + 1).toLowerCase() +
+            word.slice(hyphenIdx + 2);
+        }
       }
-    }
-    return word.charAt(0).toLowerCase() + word.slice(1)
-  }).join('-')
-})
-
+      return word.charAt(0).toLowerCase() + word.slice(1);
+    })
+    .join("-");
+});
 
 /**
  * It requires the latest node version as it uses the builtin fetch api
@@ -26,9 +33,9 @@ const modifiedTitles = titles.map(title => {
  * @returns {Promise} text component of the response
  */
 async function fetchData(url) {
-  const response = await fetch(url)
-  const body = await response.text()
-  return body
+  const response = await fetch(url);
+  const body = await response.text();
+  return body;
 }
 
 /**
@@ -37,44 +44,60 @@ async function fetchData(url) {
  */
 async function getTableContents(url) {
   try {
-    const html = await fetchData(url)
-    const $ = cheerio.load(html)
-    const header = $('header')
-    let propertyIdx = 0
-    header.next().find('thead').children().children().each((headIdx, head) => {
-      if ($(head).text() === 'Properties') {
-        propertyIdx = headIdx
-      }
-    })
+    const html = await fetchData(url);
+    const $ = cheerio.load(html);
+    const header = $("header");
+    let propertyIdx = 0;
+    header
+      .next()
+      .find("thead")
+      .children()
+      .children()
+      .each((headIdx, head) => {
+        if ($(head).text() === "Properties") {
+          propertyIdx = headIdx;
+        }
+      });
 
-    let resultMap = {}
+    let resultMap = {};
 
-    header.next().find('tbody').children().each((rowIdx, row) => {
-      resultMap[$(row).children().first().text()] = $(row).children().eq(propertyIdx).text().replace(/\/(\*.*?\*)\//gm, '').trim()
-    })
+    header
+      .next()
+      .find("tbody")
+      .children()
+      .each((rowIdx, row) => {
+        resultMap[$(row).children().first().text()] = $(row)
+          .children()
+          .eq(propertyIdx)
+          .text()
+          .replace(/\/(\*.*?\*)\//gm, "")
+          .trim();
+      });
 
-    return resultMap
-  } catch(err) {
-    console.error(err)
+    return resultMap;
+  } catch (err) {
+    console.error(err);
   }
 }
 
 /* Construct object for each page and write a JSON file */
 async function writeJSONFile() {
-  let classMap = {}
-  
+  let classMap = {};
+
   for (const page of modifiedTitles) {
-    const resultMapForEachPage = await getTableContents(`https://tailwindcss.com/docs/${page}`)
-    classMap = { ...classMap, ...resultMapForEachPage }
+    const resultMapForEachPage = await getTableContents(
+      `https://tailwindcss.com/docs/${page}`
+    );
+    classMap = { ...classMap, ...resultMapForEachPage };
   }
 
   try {
-    await fs.writeFile("tw.json", JSON.stringify(classMap, null, 2))
-    console.log('done!')
-  } catch(err) {
-      console.log("An error occured while writing JSON Object to File.");
-      return console.error(err);
+    await fs.writeFile("tw.json", JSON.stringify(classMap, null, 2));
+    console.log("done!");
+  } catch (err) {
+    console.log("An error occured while writing JSON Object to File.");
+    return console.error(err);
   }
 }
 
-await writeJSONFile()
+await writeJSONFile();
